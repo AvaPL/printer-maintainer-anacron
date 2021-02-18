3ff6b4fb3489c082f2d91fbc58879ddd0ccbe276
AvaPL <241574@student.pwr.edu.pl>
2021-02-18 20:24:05 +0100

    Fixed the invalid file name and permissions in job script.

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 PMHOME=$HOME/.printer-maintainer
 mkdir $PMHOME
 cp TestPage.pdf $PMHOME
-printf "#!/bin/bash\n\nlp $PMHOME/TestPage.pdf" | sudo tee /etc/cron.weekly/printer-maintainer.sh > /dev/null
+printf "#!/bin/bash\n\nlp $PMHOME/TestPage.pdf" | sudo tee /etc/cron.weekly/printer-maintainer > /dev/null
+sudo chmod +x /etc/cron.weekly/printer-maintainer
